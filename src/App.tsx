634cbb85e3ad4280a1583057c8f7c38893e44fe8
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-01-04 22:41:22 -0300

    motion add

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Card } from "./components/Card";
 import { InsertData } from "./components/InsertData";
 import { useApp } from "./contexts/contextApi";
 import background from "./assets/images/background.jpg";
+import { motion } from "framer-motion";
 
 function App() {
   return (
@@ -24,7 +25,19 @@ function App() {
       w="100%"
       h="100vh"
     >
-      <Text color="#fff">Search User - Github Account</Text>
+      <motion.div
+        animate={{
+          scale: [1, 1.5, 2, 1.5, 1],
+          rotate: [0, 180, 360, 270, 0],
+        }}
+        transition={{
+          duration: 2,
+        }}
+      >
+        <motion.p whileHover={{ scale: 1.2 }} style={{ color: "#fff" }}>
+          Search User - Github Account
+        </motion.p>
+      </motion.div>
       <InsertData />
     </Flex>
   );
